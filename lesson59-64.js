5aac0eb13061b6d5ea697e18af57b270081c696c
anton <antonklimovv@gmail.com>
2021-06-28 13:40:31 +0300

    65

--- lesson59-64.js
+++ lesson59-64.js
@@ -116,12 +116,179 @@
 // Решите задачу на нахождение ряда чисел, каждое из которых 
 // равно сумме трех предыдущих, а первые три числа равны 0, 1 и 2.
 
-let one = 0;
-let two = 1;
-let thr = 2;
+// let one = 0;
+// let two = 1;
+// let thr = 2;
 
-for (let i = 1; i < 10; i++) {
-    let cur = one + two + thr;
+// for (let i = 1; i < 10; i++) {
+//     let cur = one + two + thr;
+//     one = two;
+//     two = thr;
+//     thr = cur;
     
+//     console.log(cur);
+// }
+
+//Выведите на экран следующую пирамидку
+
+// let str = '';
+// for (let i = 0; i <= 10; i++) {
+//     str += 'x';
+//     document.write(str +'<br>');
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     for (let k = 0; k < i; k++) {
+//         document.write(i);
+//     }
+//     document.write('<br>');
+// }
+
+// Пирамидка из нечетных чисел
+
+// for (let i = 0; i < 6; i++) {
+//     for (let k = 0; k < i; k++) {
+//     document.write(i * 2 - 1); 
+//     }
+//     document.write('<br>');
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//     let str = '';    
+
+//     for (let j = 0; j < i; j++) {
+//         str += 'x';
+//     }
+// document.write(str + '<br>');
+// }
+
+
+// for (let i = 9; i >=1; i--) {
+//     let str = '';
     
-}
+//     for (let k = 0; k < i; k++) {
+//         document.write(i);
+        
+//     }
+//     document.write('<br>');
+// }
+
+// let arr = [];
+// let str = '';
+// for (let i = 0; i < 5; i++) {
+//     str += 'x';
+//     arr.push(str);
+// }
+// console.log(arr);
+
+
+// let arr = [];
+// let str = '';
+// for (let i = 1; i < 9; i++) {
+//     str += i;
+//     arr.push(str);   
+// }
+// console.log(arr);
+
+// let arr = [];
+// for (let i = 1; i < 9; i++) {
+//    let str = '';
+//    for (let k = 0; k < i; k++) {
+//     str += i;   
+//    } 
+//    arr.push(str); 
+// }
+// console.log(arr);
+
+// let arr = [];
+
+// for (let i = 1; i <= 5; i++) {
+//     let str = ''
+//     for (let k = 1; k <= 5; k++) {
+//        str += i;
+//     }
+//     arr.push(str);
+// }
+// console.log(arr);
+
+// let i = 1;
+// while (i <= 100) {
+//     document.write(i + '<br>');
+//     i++;
+// }
+
+// for (let i = 100; i >= 0 ; i--) {
+//     document.write(i + '<br>');
+    
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         document.write(i + '<br>');
+//     }
+    
+// }
+
+// let arr = [];
+// for (let i = 0; i < 10; i++) {
+//     arr.push('x');
+    
+// }
+// console.log(arr);
+
+// let arr = [];
+// for (let i = 1; i <= 10; i++) {
+//     arr.push(i);
+
+// }
+// console.log(arr);
+
+// for (const k of arr) {
+//     if (k < 10 && k > 2) {
+//         document.write(k);
+//     }
+    
+// }
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let flag = false;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 5) {
+//         flag = true;
+//         document.write('есть!')
+//         break; 
+//     }
+// }
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let sum = 0;
+// for (const i of arr) {
+//     sum += i;
+// }
+// document.write(sum);
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let sum = 0;
+// for (const i of arr) {
+//     let sq = i * i;
+//     sum = sum + sq; 
+// }
+// console.log(sum);
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let sum = 0;
+// let count = 0;
+
+// for (const i of arr) {
+//     count++;
+//     sum += i;
+// }
+// document.write(sum / count);
+
+// let num = 6;
+// let fck = 1;
+// for (let i = 1; i <= num; i++) {
+//     fck *= i;
+    
+// }
+// document.write(fck + '<br>');
